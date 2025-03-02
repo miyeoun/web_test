ede1b8f6cdd8313b50cec6f7b9dc922f79f57ab7
miyeoun <65397844+miyeoun@users.noreply.github.com>
2025-03-02 10:14:24 +0700

    Create color.js

---
+++ color.js
@@ -0,0 +1,24 @@
+function nightDayHandler(self){ 
+var target=document.querySelector('body');
+  if (self.value==='Night') {
+    target.style.backgroundColor='black';
+    target.style.color = 'white';
+    self.value='Day'; 
+    var alist=document.querySelectorAll('a');
+    var i=0;
+    while(i<alist.length){
+    alist[i].style.color='powderblue';  
+    i=i+1;
+       }
+     } else {
+    target.style.backgroundColor='white';
+    target.style.color = 'black';
+    self.value='Night';
+    var alist=document.querySelectorAll('a');
+    var i=0;
+    while(i<alist.length){
+    alist[i].style.color='blue';  
+    i=i+1;
+       }
+     }
+  } 
