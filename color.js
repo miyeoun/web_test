fbd91fc108e892fc45c5f512107fdf42078d8d10
miyeoun <65397844+miyeoun@users.noreply.github.com>
2025-03-02 10:19:57 +0700

    olor.js

--- color.js
+++ color.js
@@ -7,7 +7,7 @@ var target=document.querySelector('body');
     var alist=document.querySelectorAll('a');
     var i=0;
     while(i<alist.length){
-    alist[i].style.color='powderblue';  
+    alist[i].style.color='yellow';  
     i=i+1;
        }
      } else {
