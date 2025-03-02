c4ca5d18b47de1383197b50a21d0db38646ba08f
miyeoun <65397844+miyeoun@users.noreply.github.com>
2025-03-02 11:37:28 +0700

    02color.js

--- 02color.js
+++ 02color.js
@@ -1,30 +1,35 @@
-function LinksSetColor(color){
+var Links = {
+  setColor:function(color){
     var alist = document.querySelectorAll('a');
     var i = 0;
     while(i < alist.length){
       alist[i].style.color = color;
       i = i + 1;
     }
+    
+}
+var Body = {
+  setColor:function (color){
+    document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor:function (color){
+    document.querySelector('body').style.backgroundColor = color;
+   
   }
 }
-  function BodySetColor(color){
-    document.querySelector('body').style.color = color;
+function nightDayHandler(self){
+  var target = document.querySelector('body');
+  if(self.value === 'night'){
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'day';
+
+    Links.setColor('white');
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+
+    Links.setColor('blue');
   }
-  function BodySetBackgroundColor(color){
-    document.querySelector('body').style.backgroundColor = color;
-  }
-  function nightDayHandler(self){
-    var target = document.querySelector('body');
-    if(self.value === 'night'){
-      Body.setBackgroundColor('black');
-      Body.setColor('white');
-      self.value = 'day';
-      Links.setColor('powderblue');
-    } else {
-      Body.setBackgroundColor('white');
-      Body.setColor('black');
-      self.value = 'night';
-      Links.setColor('blue');
-          }
-       }
-    }
+}
