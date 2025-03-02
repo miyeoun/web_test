a5674361dbcdb7788e3b1f39544c2fbddf579896
miyeoun <65397844+miyeoun@users.noreply.github.com>
2025-03-02 11:54:32 +0700

    02color.js

--- 02color.js
+++ 02color.js
@@ -11,7 +11,8 @@ var Links = {
 }
 var Body = {
   setColor:function (color){
-    document.querySelector('body').style.color = color;
+    // document.querySelector('body').style.color = color;
+  $('body').css('color',color)
   },
   setBackgroundColor:function (color){
     document.querySelector('body').style.backgroundColor = color;
